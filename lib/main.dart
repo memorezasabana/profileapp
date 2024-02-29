f22431001104aad62f8cbc4a6b75a7039ef31fb3
Memoreza Sabana <memoreza.sabana-2022@fst.unair.ac.id>
2024-02-29 21:10:17 +0700

    tugas2

--- lib/main.dart
+++ lib/main.dart
@@ -6,184 +6,209 @@ void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
       statusBarBrightness: Brightness.dark));
-  runApp(const MyApp());
+  runApp(ProfileScreen());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ProfileScreen extends StatelessWidget {
+  ProfileScreen({super.key});
+
+  final List<Tab> myTab = [
+    const Tab(
+        text: "Profile",
+        icon: Icon(
+          CupertinoIcons.person_solid,
+          size: 30,
+        )),
+    const Tab(
+        text: "Education",
+        icon: Icon(
+          CupertinoIcons.book_solid,
+          size: 30,
+        )),
+    const Tab(
+        text: "Story",
+        icon: Icon(
+          CupertinoIcons.news_solid,
+          size: 30,
+        )),
+    const Tab(
+        text: "Skills",
+        icon: Icon(
+          CupertinoIcons.gear_solid,
+          size: 30,
+        )),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BELAJAR FLUTTER',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: 'ProductSans',
       ),
-      home: const ProfileScreen(),
-    );
-  }
-}
-
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 70,
-        title: const Text(
-          "BISMILLAH BELAJAR FLUTTER",
-          style: TextStyle(fontFamily: "ProductSans", color: Colors.white),
-        ),
-        centerTitle: true,
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20)),
-              gradient: LinearGradient(
-                  colors: [const Color(0xff42A3A7), Colors.teal.shade200],
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter)),
-        ),
-      ),
-      body: Padding(
-          padding: const EdgeInsets.only(right: 10, left: 10),
-          child: ListView(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: myTab.length,
+        child: Scaffold(
+          appBar: AppBar(
+            toolbarHeight: 70,
+            title: const Text(
+              "Hello Viewer!",
+              style: TextStyle(
+                fontFamily: "ProductSans",
+                color: Colors.white,
+              ),
+            ),
+            centerTitle: true,
+            flexibleSpace: Container(
+              decoration: const BoxDecoration(
+                // borderRadius: const BorderRadius.only(
+                //     bottomLeft: Radius.circular(20),
+                //     bottomRight: Radius.circular(20)),
+                color: Color(0xff42A3A7),
+                // gradient: LinearGradient(colors: [
+                //   const Color(0xff42A3A7),
+                //   Color.fromARGB(255, 0, 85, 76)
+                // ], begin: Alignment.bottomCenter, end: Alignment.topCenter)
+              ),
+            ),
+            bottom: TabBar(
+              labelStyle: const TextStyle(fontWeight: FontWeight.normal),
+              labelColor: Colors.white,
+              unselectedLabelColor: Colors.white60,
+              indicatorColor: Colors.white,
+              indicatorWeight: 5,
+              // indicatorPadding: EdgeInsets.all(10),
+              // indicator: BoxDecoration(
+              //     color: Colors.amber,
+              //     border: Border(
+              //         bottom: BorderSide(
+              //       color: Colors.black,
+              //       width: 5,
+              //     ))),
+              tabs: myTab,
+            ),
+          ),
+          body: TabBarView(
             children: [
-              const SizedBox(height: 25),
-              CircleAvatar(
-                radius: 60,
-                child: ClipOval(
-                  child: Image.asset('assets/images/user.jpg'),
-                ),
-              ),
-              const Align(
-                alignment: Alignment.center,
-                child: Text(
-                  "Memoreza Sabana",
-                  style: TextStyle(
-                    fontSize: 22,
-                    height: 3,
-                    fontFamily: 'ProductSans',
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: Text(
-                  "1. Biodata Mahasiswa",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontFamily: 'ProductSans',
-                    height: 2,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              itemProfile('NIM', '187221007', CupertinoIcons.creditcard),
-              const SizedBox(height: 10),
-              itemProfile('Family', 'Anak ke-2 dari 2 bersaudara',
-                  CupertinoIcons.person),
-              const SizedBox(height: 10),
-              itemProfile('Alamat', 'Kota Surabaya', CupertinoIcons.location),
-              const SizedBox(height: 10),
-              itemProfile('Tempat, Tanggal Lahir', 'Tuban, 16 Oktober 2003',
-                  CupertinoIcons.calendar),
-              const SizedBox(height: 10),
-              itemProfile('Hobi', 'Traveling', CupertinoIcons.heart),
-              const SizedBox(height: 10),
-              itemDesc('Deskripsi Diri',
-                  'Individu yang kreatif, ramah, dan penuh semangat, selalu melihat dunia dengan mata yang penuh warna dan penuh dengan ide-ide baru'),
-              const SizedBox(height: 10),
-              const Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: Text(
-                  "2. Histori Pendidikan",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontFamily: 'ProductSans',
-                    height: 2,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              itemHistPendidikan(
-                  'Sekolah Dasar (SD)', 'SD Negeri 1 Sumurcinde'),
-              const SizedBox(height: 10),
-              itemHistPendidikan(
-                  'Sekolah Menengah Pertama (SMP)', 'SMP Negeri 1 Bojonegoro'),
-              const SizedBox(height: 10),
-              itemHistPendidikan(
-                  'Sekolah Menengah Kejuruan (SMK)', 'SMK Telkom Malang'),
-              const SizedBox(height: 10),
-              itemHistPendidikan('Perguruan Tinggi', 'Universitas Airlangga'),
-              const SizedBox(height: 10),
-              const Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: Text(
-                  "3. Pengalaman Studi",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontFamily: 'ProductSans',
-                    height: 2,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              itemDesc('Suka Duka di SI-FST',
-                  'Kesempatan untuk mempelajari berbagai konsep dan teknologi terkini dalam bidang sistem informasi yang menarik. Namun, di sisi lain, terdapat juga tantangan seperti tugas yang menuntut, jadwal yang padat'),
-              const SizedBox(height: 10),
-              const Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: Text(
-                  "4. Keahlian Mahasiswa",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontFamily: 'ProductSans',
-                    height: 2,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              itemProfile(
-                  'Keahlian', 'UI/UX and Graphic Design', CupertinoIcons.gear),
-              const SizedBox(height: 20),
-              Row(
-                children: [
-                  const Padding(padding: EdgeInsets.only(left: 20)),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(12),
-                    child: const Image(
-                      width: 180,
-                      image: NetworkImage(
-                          'https://cdn.dribbble.com/users/6485793/screenshots/19334768/media/4a2f6e5de152ed49eec66fa322affaae.png'),
-                    ),
-                  ),
-                  const SizedBox(width: 20),
-                  const Flexible(
-                    child: Text(
-                      textAlign: TextAlign.start,
-                      overflow: TextOverflow.clip,
-                      softWrap: true,
-                      "(cr. dribbble/memorezasabana) Project intern at PT. Aksamedia Mulia",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontFamily: 'ProductSans',
+              Padding(
+                  padding: const EdgeInsets.only(right: 10, left: 10),
+                  child: ListView(
+                    children: [
+                      const SizedBox(height: 25),
+                      CircleAvatar(
+                        radius: 60,
+                        child: ClipOval(
+                          child: Image.asset('assets/images/user.jpg'),
+                        ),
                       ),
-                    ),
-                  ),
+                      const Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Memoreza Sabana",
+                          style: TextStyle(
+                            fontSize: 22,
+                            height: 3,
+                            fontFamily: 'ProductSans',
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      // const Padding(
+                      //   padding: EdgeInsets.only(left: 20),
+                      //   child: Text(
+                      //     "1. Biodata Mahasiswa",
+                      //     style: TextStyle(
+                      //       fontSize: 18,
+                      //       fontFamily: 'ProductSans',
+                      //       height: 2,
+                      //     ),
+                      //   ),
+                      // ),
+                      const SizedBox(height: 10),
+                      itemProfile(
+                          'NIM', '187221007', CupertinoIcons.creditcard),
+                      const SizedBox(height: 10),
+                      itemProfile('Family', 'Anak ke-2 dari 2 bersaudara',
+                          CupertinoIcons.person),
+                      const SizedBox(height: 10),
+                      itemProfile(
+                          'Alamat', 'Kota Surabaya', CupertinoIcons.location),
+                      const SizedBox(height: 10),
+                      itemProfile('Tempat, Tanggal Lahir',
+                          'Tuban, 16 Oktober 2003', CupertinoIcons.calendar),
+                      const SizedBox(height: 10),
+                      itemProfile('Hobi', 'Traveling', CupertinoIcons.heart),
+                      const SizedBox(height: 10),
+                      itemDesc('Deskripsi Diri',
+                          'Individu yang kreatif, ramah, dan penuh semangat, selalu melihat dunia dengan mata yang penuh warna dan penuh dengan ide-ide baru'),
+                      const SizedBox(height: 20),
+                    ],
+                  )),
+              ListView(
+                children: [
+                  const SizedBox(height: 25),
+                  itemHistPendidikan(
+                      'Sekolah Dasar (SD)', 'SD Negeri 1 Sumurcinde'),
+                  const SizedBox(height: 10),
+                  itemHistPendidikan('Sekolah Menengah Pertama (SMP)',
+                      'SMP Negeri 1 Bojonegoro'),
+                  const SizedBox(height: 10),
+                  itemHistPendidikan(
+                      'Sekolah Menengah Kejuruan (SMK)', 'SMK Telkom Malang'),
+                  const SizedBox(height: 10),
+                  itemHistPendidikan(
+                      'Perguruan Tinggi', 'Universitas Airlangga'),
+                  const SizedBox(height: 10),
                 ],
               ),
-              const SizedBox(height: 20),
-              itemDesc('Portofolio: Landing Page Web',
-                  'Real project yang saya kerjakan sebuah desain landing page untuk website Aneka Corak Cipta, suatu perusahaan yang menawarkan layanan desain arsitektural, workshop mekanikal, dan rental alat berat dalam satu platform yang menarik dan fungsional'),
-              const SizedBox(height: 30),
+              ListView(children: [
+                const SizedBox(height: 25),
+                itemDesc('Suka Duka di SI-FST',
+                    'Ketika awalnya saya memulai kuliah di jurusan Sistem Informasi di Universitas Airlangga, saya sangat antusias dengan segala kesempatan yang ditawarkan untuk mendalami konsep dan teknologi terkini dalam dunia informasi. Mulai dari materi perkuliahan hingga proyek-proyek praktis, semuanya terasa begitu menarik dan memicu semangat belajar saya. \nTapi, seiring berjalannya waktu, saya mulai menyadari bahwa tidak semua yang tampak indah itu mudah. Tantangan-tantangan seperti tugas kuliah yang menuntut dan jadwal yang padat seringkali membuat saya merasa tertekan. Terkadang, saya bahkan merasa ragu dengan kemampuan saya sendiri. Terutama saat mendekati deadline tugas atau menjelang ujian. \nMeskipun begitu, saya bersyukur atas setiap tantangan yang saya hadapi. Mereka semua memberikan saya pelajaran berharga. Saya belajar bagaimana mengelola waktu dengan lebih efisien, meningkatkan kemampuan multitasking, dan memperdalam pemahaman terhadap materi kuliah. Selain itu, saya juga belajar bekerja sama dalam tim, mengasah keterampilan komunikasi, dan mengembangkan ketangguhan mental untuk menghadapi tekanan. \nTidak hanya itu, kuliah di jurusan Sistem Informasi Universitas Airlangga juga memberi saya kesempatan untuk terlibat dalam berbagai kegiatan di luar kelas. Mulai dari seminar dan workshop hingga berbagai kompetisi teknologi, saya aktif dalam memanfaatkan setiap kesempatan untuk memperluas jaringan dan mengembangkan keterampilan tambahan. \nMeskipun perjalanan kuliah ini tidak selalu mudah, saya bersyukur atas semua pengalaman yang telah saya dapatkan. Mereka semua telah membentuk saya menjadi pribadi yang lebih tangguh, percaya diri, dan siap menghadapi tantangan di dunia nyata setelah lulus nanti. Kesempatan untuk belajar dan berkembang di jurusan Sistem Informasi Universitas Airlangga adalah anugerah yang saya hargai dan akan saya manfaatkan sebaik mungkin.'),
+                const SizedBox(height: 10),
+              ]),
+              ListView(children: [
+                const SizedBox(height: 25),
+                itemProfile('Keahlian', 'UI/UX and Graphic Design',
+                    CupertinoIcons.gear),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    const Padding(
+                        padding: EdgeInsets.only(left: 20, right: 20)),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: const Image(
+                        width: 180,
+                        image: NetworkImage(
+                            'https://cdn.dribbble.com/users/6485793/screenshots/19334768/media/4a2f6e5de152ed49eec66fa322affaae.png'),
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    const Flexible(
+                      child: Text(
+                        textAlign: TextAlign.start,
+                        overflow: TextOverflow.clip,
+                        softWrap: true,
+                        "(cr. dribbble/memorezasabana) Project intern at PT. Aksamedia Mulia",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'ProductSans',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                itemDesc('Portofolio: Landing Page Web',
+                    'Real project yang saya kerjakan sebuah desain landing page untuk website Aneka Corak Cipta, suatu perusahaan yang menawarkan layanan desain arsitektural, workshop mekanikal, dan rental alat berat dalam satu platform yang menarik dan fungsional'),
+                const SizedBox(height: 30),
+              ]),
             ],
-          )),
+          ),
+        ),
+      ),
     );
   }
 
@@ -203,7 +228,7 @@ class ProfileScreen extends StatelessWidget {
       child: ListTile(
         title: Text(
           title,
-          style: const TextStyle(fontSize: 14, fontFamily: 'ProductSans'),
+          style: const TextStyle(fontSize: 14, fontFamily: 'ProductSans', fontWeight: FontWeight.bold),
         ),
         subtitle: Text(
           subtitle,
@@ -232,11 +257,12 @@ class ProfileScreen extends StatelessWidget {
         title: Text(
           title,
           textAlign: TextAlign.center,
-          style: const TextStyle(fontSize: 18, fontFamily: 'ProductSans'),
+          style: const TextStyle(fontSize: 18, fontFamily: 'ProductSans', fontWeight: FontWeight.bold),
         ),
         subtitle: Text(
           subtitle,
-          style: const TextStyle(fontSize: 16, fontFamily: 'ProductSans'),
+          textAlign: TextAlign.justify,
+          style: const TextStyle(fontSize: 16, fontFamily: 'ProductSans',),
         ),
       ),
     );
@@ -258,7 +284,7 @@ class ProfileScreen extends StatelessWidget {
       child: ListTile(
         title: Text(
           title,
-          style: const TextStyle(fontSize: 14, fontFamily: 'ProductSans'),
+          style: const TextStyle(fontSize: 14, fontFamily: 'ProductSans', fontWeight: FontWeight.bold),
         ),
         subtitle: Text(
           subtitle,
